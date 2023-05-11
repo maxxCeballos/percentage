d374d91b9f7b5b4e522244550b4b44be6475a525
Maxi Ceballos <maxi@MBP-de-Maxi.fibertel.com.ar>
2023-05-11 03:30:34 -0300

    anda algo ya de conexion a cache

--- src/main/java/com/tenpo/profit/infraestructure/adapters/output/rest/percentage/PercentageRestAdapter.java
+++ src/main/java/com/tenpo/profit/infraestructure/adapters/output/rest/percentage/PercentageRestAdapter.java
@@ -2,8 +2,11 @@ package com.tenpo.profit.infraestructure.adapters.output.rest.percentage;
 
 import com.tenpo.profit.application.ports.output.GetPercentage;
 import com.tenpo.profit.infraestructure.adapters.output.rest.percentage.data.PercentageDTO;
+import org.springframework.cache.annotation.CacheConfig;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.client.RestTemplate;
 
+@CacheConfig(cacheNames = "profitCache")
 public class PercentageRestAdapter implements GetPercentage {
 
     RestTemplate client = new RestTemplate();
@@ -11,6 +14,7 @@ public class PercentageRestAdapter implements GetPercentage {
     public PercentageRestAdapter() {}
 
     @Override
+    @Cacheable(cacheNames = "profits")
     public PercentageDTO getIncrementPercentage() {
 
 //        return client.getForObject("http://localhost:3000/percentage", PercentageDTO.class);
